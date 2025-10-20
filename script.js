d0a39211ba09264bde72cacc3ab82c8b85cdbe1d
Alyssa <alyssa.goldhart@gmail.com>
2025-10-20 09:18:47 -0400

    fix hover effect to only target child elements of grid + calc flex values based upon user input

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ const grid = document.querySelector('.grid');
 
 function createGrid(row, column) {
   for (let i = 0; i < row * column ; i++) {
+    const flexPercentage = 100/(row + 1);
+    const flexBasis = flexPercentage.toString();
+    const flexValue = `1 1 ${flexBasis}%`;
     const square = document.createElement("div");
-    square.classList.add('square-div');
+    square.classList.add('squareDiv');
     grid.appendChild(square);
+    square.style.border = "1px solid black";
+    square.style.flex = flexValue;
+    square.addEventListener("mouseover", (event) => {
+      event.target.style.backgroundColor = "black";
+    });
   }
 };
 
 createGrid(16, 16);
 
-grid.addEventListener("mouseover", (event) => {
-  event.target.style.backgroundColor = "orange";
-});
