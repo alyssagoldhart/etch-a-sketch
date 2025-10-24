cec2115d70de6a3b9ab2affb6d46e692185ef6d7
Alyssa <alyssa.goldhart@gmail.com>
2025-10-24 16:24:22 -0400

    added function to change grid button

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 const grid = document.querySelector(".grid");
 const button = document.querySelector("button");
 
-function createGrid(row, column) {
-  for (let i = 0; i < row * column ; i++) {
+function createGrid(row) {
+  for (let i = 0; i < row ** 2 ; i++) {
     const flexPercentage = 100/(row + 1);
     const flexBasis = flexPercentage.toString();
-    const flexValue = `1 1 ${flexBasis}%`;
-    const square = document.createElement("div");
-    square.classList.add("squareDiv");
-    grid.appendChild(square);
-    square.style.border = "1px solid black";
-    square.style.flex = flexValue;
-    square.addEventListener("mouseover", (event) => {
-      event.target.style.backgroundColor = "black";
-    });
-  }
+    const flexValue = `1 0 ${flexBasis}%`;
+    const gridSquare = document.createElement("div");
+    gridSquare.classList.add("gridSquare");
+    grid.appendChild(gridSquare);
+    gridSquare.style.border = "1px solid black";
+    gridSquare.style.flex = flexValue;
+    gridSquare.addEventListener("mouseover", (event) => {
+      event.target.style.backgroundColor = "orange";
+    })
+   console.log(row); 
+   console.log(flexPercentage);
+    
+  };
+  
 };
 
-createGrid(16, 16);
+createGrid(16);
 
-button.addEventListener("click", (event) => prompt('Enter a number between 4 and 100'));
+function clearGrid() {
+  while (grid.firstChild) {
+  grid.removeChild(grid.firstChild);
+  };
+};
+
+function changeGrid() {
+  clearGrid();
+  const gridSize = prompt('Enter a number between 4 and 100');
+  const newGridSize = Number(gridSize);
+  createGrid(newGridSize);
+};
